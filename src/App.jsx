58d1c02341d0600793ad258cfda4c7a56167970f
zainsajjad903 <zainsajjad.903@gmail.com>
2025-11-10 11:41:21 -0800

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="underline underline-offset-1">
+      {" "}
+      Hallo zain how are you? whats going on bro
+    </div>
+  );
+}
